d2e7f477484def0c81d68df69804cf34d47ec105
Dan Clair <danclair@me.com>
2020-03-11 16:11:19 -0500

    Add RESTful conventions to controller, routes, and index and show files

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,40 @@
+class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render "index.json.jb"
+  end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params[:id])
+    contact.destroy
+    render json: { message: "Contact successfully deleted!" }
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+contact = Contact.new({ first_name: "Hermione", middle_name: "Bookworm", last_name: "Granger", email: "hermione@example.com", phone_number: "312-123-1234" })
+contact.save
+contact = Contact.new({ first_name: "Ron", middle_name: "Ginger", last_name: "Weasley", email: "ron@example.com", phone_number: "312-123-1235" })
+contact.save
+contact = Contact.new({ first_name: "Harry", middle_name: "Scarhead", last_name: "Potter", email: "harry@example.com", phone_number: "312-123-1236" })
+contact.save
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
